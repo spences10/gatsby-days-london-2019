810cb2dfec7c2b1db3450fa8162e88d497864be3
spences10 <spences10apps@gmail.com>
2019-10-02 06:47:41 +0100

    add theme options

--- src/theme.js
+++ src/theme.js
@@ -8,7 +8,7 @@ const Slides = ({ children }) => {
     position: 'absolute',
     right: 0,
     top: 0,
-    textAlign: 'right'
+    textAlign: 'right',
   }
 
   return (
@@ -35,6 +35,79 @@ const Provider = props => {
   )
 }
 
+const fonts = () => {
+  const list = [
+    'Nunito:400,700|',
+    'Poppins:400,700|',
+    'Cherry+Swash:400,700|',
+    'Montserrat+Alternates:400,700|',
+    'Vollkorn:400,700|',
+  ]
+  return list.join('')
+}
+
 export default {
-  Provider
+  googleFont: `https://fonts.googleapis.com/css?family=${fonts()}`,
+  fonts: {
+    body: 'Poppins, sans-serif',
+  },
+  text: {
+    heading: {
+      color: '#fff',
+      fontFamily: 'Montserrat Alternates',
+      margin: '0 auto 0.5rem',
+      textAlign: 'center',
+    },
+  },
+  colors: {
+    text: '#000',
+    background: 'rebeccapurple',
+    link: '#000',
+    heading: '#000',
+    quote: '#000',
+    pre: '#333',
+    preBackground: 'rgb(245, 242, 240)',
+    code: '#333',
+    codeBackground: 'transparent',
+    figcaptionBackground: 'rebeccapurple',
+    figcaption: '#fff',
+  },
+  styles: {
+    body: {},
+    Slide: {
+      textAlign: 'center',
+    },
+    p: {
+      fontSize: '3rem',
+      margin: '1rem 0',
+      textAlign: 'center',
+    },
+    h1: {
+      fontSize: '5rem',
+      marginBottom: '0.25rem',
+    },
+    h2: {
+      fontSize: '4rem',
+      margin: '0.5rem 0',
+    },
+    figure: {
+      textAlign: 'left',
+    },
+    code: {
+      fontSize: 'inherit',
+    },
+    figcaption: {
+      fontSize: '1.2rem',
+    },
+    li: {
+      marginBottom: '0.5rem',
+      textAlign: 'left',
+    },
+    img: {
+      display: 'inline-block',
+      objectFit: 'contain',
+      maxHeight: '100vh',
+    },
+  },
+  Provider,
 }
