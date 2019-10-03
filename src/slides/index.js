e9c14b1c462bdffc5b26a9f27629e8685c4ac97d
spences10 <spences10apps@gmail.com>
2019-10-03 16:23:22 +0100

    keynote

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,6 +1,17 @@
 import Agenda from './agenda.mdx'
+import IncrementalBuilds from './incremental-builds.mdx'
 import Intro from './intro.mdx'
+import Keynote from './keynote.mdx'
 import Poll from './poll.mdx'
 import Preamble from './preamble.mdx'
+import Resources from './resources.mdx'
 
-export { Intro, Poll, Agenda, Preamble }
+export {
+  Intro,
+  Poll,
+  Agenda,
+  Preamble,
+  Resources,
+  IncrementalBuilds,
+  Keynote,
+}
